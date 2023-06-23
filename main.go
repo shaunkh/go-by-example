b1e21ae377835e7d59a76f8d3b008d118a8dd712
shaunkh <shaun.khundker@gmail.com>
2023-06-23 15:47:53 +0200

    what you can do type switch?

--- main.go
+++ main.go
@@ -24,5 +24,17 @@ func main() {
 	default:
 		fmt.Println("It's a weekday")
 	}
-
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
