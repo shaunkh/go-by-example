9589dbd1557b25a553dd8f552be48c5c4e14e3c4
shaunkh <shaun.khundker@gmail.com>
2023-06-23 15:40:59 +0200

    using time + switch

--- main.go
+++ main.go
@@ -2,30 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i = i + 1
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
 	}
 
-	for j := 7; j <= 9; j++ {
-		fmt.Println(j)
-	}
-
-	for {
-		fmt.Println("loop")
-		break
-	}
-
-	for n := 0; n <= 5; n++ {
-		if n%2 == 0 {
-			continue // continue to next iteration
-		}
-		fmt.Println(n)
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
 	}
 
 }
